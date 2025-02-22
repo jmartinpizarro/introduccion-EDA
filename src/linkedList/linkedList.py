44ed3cf07124643cd1436f3d49080f05bf94d626
Javier Martín <jmartinpizarro04@gmail.com>
2025-02-22 12:25:17 +0100

    Basic Linked List structure

---
+++ src/linkedList/linkedList.py
@@ -0,0 +1,83 @@
+class Node():
+    "Implementación de un Nodo"
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList():
+    "Simple Linked List structure"
+    def __init__(self):
+        self.head = None
+        self.length = 0
+
+    def __str__(self):
+        values = []
+        c = self.head
+        while c is not None:
+            values.append(str(c.value))
+            c = c.next
+        return " -> ".join(values)
+
+    def isEmpty(self):
+        return self.length == 0
+    
+    def insert(self, value):
+        "Inserta un elemento en la última posición de la lista: append()"
+        n = Node(value)
+
+        if self.isEmpty():  # isEmpty
+            self.head = n
+            self.length += 1
+            return 1
+        else:
+            current = self.head
+            while current.next is not None:
+                current = current.next
+            current.next = n
+        
+        self.length += 1
+        return 1
+    
+    def remove(self, value):
+        "Borra el primer elemento de la lista que tenga dicho valort"
+        if self.isEmpty():
+            return 0
+        
+        c = self.head
+        prev = None
+
+        if self.length == 1: # Solo existe un elemento en la lista
+            self.head = None
+            self.length = 0
+            return 1
+        
+        if c.value == value: # head es el valor a eliminar
+            self.head = c.next
+            c.next = None
+            self.length -= 1
+            return 1
+
+        while c is not None: # mientras que exista un nodo
+            if c.value == value:
+                prev.next = c.next
+                c.next = None
+                self.length -= 1
+                return 1
+
+            prev = c    
+            c = c.next
+            
+
+        return 0 
+
+
+        
+ll = LinkedList()
+ll.insert(1)
+ll.insert(2)
+ll.insert(3)
+ll.insert(4)
+ll.insert(5)
+ll.insert(6)
+
+print(ll)
